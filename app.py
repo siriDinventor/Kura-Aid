d8ba7bccf6092127ec80831a264eba2dacc0616f
siriDinventor <siriDinventor@gmail.com>
2022-11-25 15:51:35 +1100

    another commit

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = FastAPI()
 fake_db = {
     "09032097223":
         {
-            "location": "Ikpoba Okha, Edo Satte",
+            "LGA": "Oredo",
+            "state": "Edo",
             "name": "",
             "phone": "902346829",
             "joined": 897643289,
@@ -20,35 +21,64 @@ locationDictionary = {
     "Edo": ["Oredo", "Ikpoba-Okha"]
 }
 
-
-def location(text, phone_number):
-    print(text)
-    loc = locationDictionary.get(text, None)
-    print(loc)
-    response = "CON Which LGA are you in?\n"
-    s = text
-    s_list = s.split("*")
-    if s_list[0] == fake_db.get(phone_number)["name"]:
-        loc = locationDictionary.get(s_list[1])
+def make_lga(p_response, lga_list, part = 0):
+    i = 0
+    if len(lga_list) < 8:
         j = 1
-        for i in loc:
-            response += f"{j}. {i}" + "\n"
+        for i in lga_list:
+            p_response += f"{j}. {i}\n"
             j += 1
-            print(response)
-        return response
-    if loc == None and text == "":
-        return "CON please provide your state"
+        return p_response 
+
+
+def getState(text, phone_number):
+    is_exist = ""
+    if text == "":
+        return "CON will you type your state\nmy friend!"
+    text = text.split("*")[-1]
+    for i in locationDictionary:
+        if str(text).casefold() == str(i).casefold():
+            is_exist = i
+    
+    if is_exist:
+        user = fake_db.get(phone_number)
+        user.update({"state": is_exist})
+        fake_db.update({str(phone_number): user})
+
+        res = "CON Which LGA are you in?\n"
+        loc = locationDictionary[is_exist]
+        return make_lga(res, loc, 0)
     else:
-        for i in loc:
-            response += i + "\n"
-            print(response)
+        return "CON Learn how to spell your state"
+
+def getLga(number, phone_number):
+    if number == "":
+        return "CON Please will you type your number\n"
+    number = number.split("*")[-1]
+    user = fake_db.get(phone_number)
+    main_list = locationDictionary[user.get("state")]
+    p = user["part"]
+    if len(main_list) < int(number) and int(number) < 1:
+        return "CON Please will you type your number\n"
+    else:
+        l = main_list[int(number) - 1]
+        user = fake_db.get(phone_number)
+        user.update({"LGA": l})
+        fake_db.update({str(phone_number): user})
+        response = "CON Welcome to Kura where yo have access to quality health services\n"
+        response += "1. Specialist \n"
+        response += "2. Reminder \n"
+        response += "3. Pharmacy \n"
+        response += "4. Hospital \n"
+        response += "5. Caretaker\n"
         return response
-    return response
 def unregisteredUser(text, phone):
     user = fake_db.get(phone)
     user.update({"name": text})
     fake_db.update({str(phone): user})
     return "CON Which State do you reside:"
+
+
 @app.get("/test")
 def check():
     return ('please work')
@@ -65,13 +95,14 @@ async def index(request: Request):
     if user:
         if user["name"] == "":
             return unregisteredUser(text, phone_number)
-        elif user["location"] == "":
-            return location(text, phone_number)
+        elif user["state"] == "":
+            return getState(text, phone_number)
+        elif user["LGA"] == "":
+            return getLga(text, phone_number)
         else:
             s = text
             s_list = s.split("*")
             # e.g s_list = ['salman', '1']
-
             j = 0
             real_string = ""
             for i in s_list:
@@ -79,7 +110,6 @@ async def index(request: Request):
                     for k in s_list[j:]:
                         real_string += k + "*"
                         print(real_string)
-
                     # real_string = "".join(s_list[j:])
                     # print(real_string)
 
@@ -118,6 +148,9 @@ async def index(request: Request):
             {
                 phone_number: {
                     "location": "",
+                    "state": "",
+                    "LGA": "",
+                    "part": 0,
                     "name": "",
                     "phone": phone_number,
                     "joined": time.time(),
