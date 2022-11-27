5e1c7f607aa40378d43e59ec9703d6b6681ca9b5
siriDinventor <siriDinventor@gmail.com>
2022-11-27 12:26:26 +1100

    all my changes

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ fake_db = {
             "LGA": "Oredo",
             "state": "Edo",
             "name": "",
+            "Profession": "",
             "phone": "902346829",
             "joined": 897643289,
             "history": [
@@ -18,11 +19,13 @@ fake_db = {
         },
 }
 locationDictionary = {
-    "Edo": ["Akoko-Edo", "Etsako East", "Etsako Central", "Etsako West", "Owan West", "Esan North-East", "Esan Central", "Esan South-east",  "Ovia South-West", "Ovia North-East", "Igueben", "Egor", "Uhunmwonde", "Oredo", "Ikpoba-Okha", "Orhionmwon", ]
+    "Edo": ["Orhionmwon", "Etsako East"]
 }
 HospitalDictionary = {
-    "LGA": ["hb"]
+    "Orhionmwon": ["1. WELL-FORT", "2. Our ", "3. Enoma Medical Center "]
 }
+prof_list = ["1. Caretaker", "2. Doctor", "3. User"]
+
 def make_lga(p_response, lga_list, part = 0):
     i = 0
     if len(lga_list) < 8:
@@ -32,11 +35,11 @@ def make_lga(p_response, lga_list, part = 0):
             j += 1
         return p_response 
 def Hospital(LGA):
-    LGA = fake_db.get(phone_Number["LGA"])
     hospitalList = HospitalDictionary.get(LGA)
-    response = "CON These are the list of hospitals:"
+    response = "CON These are the list of hospitals: \n"
     for i in hospitalList:
         response += f"{i}\n"
+    response += "0. Next"
     return response
 def getState(text, phone_number):
     is_exist = ""
@@ -51,16 +54,17 @@ def getState(text, phone_number):
         user = fake_db.get(phone_number)
         user.update({"state": is_exist})
         fake_db.update({str(phone_number): user})
-
         res = "CON Which LGA are you in?\n"
         loc = locationDictionary[is_exist]
-        return make_lga(res, loc, 0)
+        m =  make_lga(res, loc, 0)
+        m += "0. next"
+        return m
     else:
         return "CON spell your State correctly"
 
 def getLga(number, phone_number):
     if number == "":
-        return "CON Please will you type your number\n"
+        return "CON Which LGA do you reside in? \n "
     number = number.split("*")[-1]
     user = fake_db.get(phone_number)
     main_list = locationDictionary[user.get("state")]
@@ -73,11 +77,11 @@ def getLga(number, phone_number):
         user.update({"LGA": l})
         fake_db.update({str(phone_number): user})
         response = "CON Welcome to Kura where you have access to quality health services\n"
-        response += "1. Specialist\n"
-        response += "2. Reminder \n"
-        response += "3. Pharmacy \n"
-        response += "4. Hospital \n"
-        response += "5. Caretaker\n"
+        response += "1. Reminder \n"
+        response += "2. Pharmacy \n"
+        response += "3. Hospital \n"
+        response += "4. Caretaker\n"
+        response += "5. Payment\n"
         return response
 def unregisteredUser(text, phone):
     user = fake_db.get(phone)
@@ -86,6 +90,31 @@ def unregisteredUser(text, phone):
     return "CON Which State do you reside:"
 
 
+def professionFunc(phone_number, number):
+    print(number)
+    if number == "":
+        response = "CON State Your Profession: \n"
+        response += "1. Caretaker \n" 
+        response += "2. Doctor \n" 
+        response += "3. User \n" 
+        return response
+    user = fake_db.get(phone)
+    if len(prof_list) < int(number) and int(number) < 1:
+        return "CON Invalid Number\n"
+    else:
+        user = fake_db.get(phone_number)
+        
+        l = prof_list[int(number) - 1]
+        user.update({"Professiion": l})
+        fake_db.update({str(phone_number): user})
+        response = "CON Welcome to Kura where you have access to quality health services\n"
+        response += "1. Specialist \n"
+        response += "2. Reminder \n"
+        response += "3. Pharmacy \n"
+        response += "4. Hospital \n"
+        response += "5. Caretaker\n"
+        response += "6. Payment\n"
+        return response
 @app.get("/test")
 def check():
     return ('please work')
@@ -116,7 +145,6 @@ async def index(request: Request):
                 if i.isdigit():
                     for k in s_list[j:]:
                         real_string += k + "*"
-                        print(real_string)
                     # real_string = "".join(s_list[j:])
                     # print(real_string)
 
@@ -128,39 +156,42 @@ async def index(request: Request):
 
             if text == "":
                 response = "CON Welcome to Kura where yo have access to quality health services\n"
-                response += "1. Specialist \n"
-                response += "2. Reminder \n"
-                response += "3. Pharmacy \n"
-                response += "4. Hospital \n"
-                response += "5. Caretaker\n"
+                response += "1. Reminder \n"
+                response += "2. Pharmacy \n"
+                response += "3. Hospital \n"
+                response += "4. Caretaker\n"
+                response += "5. Payment Method\n"
                 return response
-            elif text == "1":
-                response = "END welldone you can read english"
+            elif text == "1*1":
+                response = "CON what do you want to reminded off?.. type away"
+            elif text == "1*2":
+                response = "CON what do you want to reminded off?.. type away"
                 return response
             elif text == "2":
-                response = "CON what type of reminder do you want?\n1. One time\n2. Repeat"
-                return response
-            elif text == "2*1":
-                response = "CON what do you want to reminded off?.. type away"
-            elif text == "2*2":
-                response = "CON what do you want to reminded off?.. type away"
-                return response
-            elif text == "3":
                 response = "Pharmacy"
-            elif text == "4":
+            elif text == "3":
+                LGA = user.get("LGA")
                 return Hospital(LGA)
-            elif text == "5":
+            elif text == "3*1":
+                response = "CON 1. Obsetrician \n"
+                response += "2. Paediatrician \n"
+                response += "3. Gynaecologist"
+                response += "4. Nutritionist"
+                response += "5. Dentist"
+
+                return response
+            elif text == "4":
                 response = "Caretaker"
                 return response
             else:
                 return "Invalid Input"
-
     else:
         fake_db.update(
             {
                 phone_number: {
                     "location": "",
                     "state": "",
+                    "Profession": "",
                     "LGA": "",
                     "part": 0,
                     "name": "",
@@ -172,3 +203,4 @@ async def index(request: Request):
                 }}
         )
         return "CON please type your name"
+
